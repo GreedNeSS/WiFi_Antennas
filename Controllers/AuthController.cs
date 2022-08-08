e180a71f5e9043b7e7b3b8b712b3e56d09d63380
GreedNeSS <ruslan199126@gmail.com>
2022-08-08 19:42:51 +0300

    Added AuthController.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using WiFi_Antennas_Win.Models;
+
+namespace WiFi_Antennas_Win.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
